29d3ac1073e87c32e37acdd59908ba5317c0d560
Lovro Markovic <lovro.markovic@fer.hr>
2021-05-13 23:06:13 +0200

    Formatting

--- include/uav_ros_control/control/ModelPredictiveControl.hpp
+++ include/uav_ros_control/control/ModelPredictiveControl.hpp
@@ -10,24 +10,24 @@ namespace uav_ros_control {
 class ModelPredictiveControl : public controller_interface
 {
 public:
-  
   /**
    * @brief Construct a new Model Predictive Control object.
-   * 
+   *
    */
   ModelPredictiveControl();
 
   /* Override all pure virtual methods */
-  void initialize(ros::NodeHandle &parent_nh,
-    const std::string name,
-    const double uav_mass) override;
-  
-  bool activate(const mavros_msgs::AttitudeTargetConstPtr &last_attitude_cmd) override;
-  
+  void initialize(ros::NodeHandle&  parent_nh,
+                  const std::string name,
+                  const double      uav_mass) override;
+
+  bool activate(const mavros_msgs::AttitudeTargetConstPtr& last_attitude_cmd) override;
+
   void deactivate() override;
-  
-  const mavros_msgs::AttitudeTarget update(const nav_msgs::OdometryConstPtr &uav_state,
-    const trajectory_msgs::MultiDOFJointTrajectoryPointConstPtr &last_position_cmd)
+
+  const mavros_msgs::AttitudeTarget update(
+    const nav_msgs::OdometryConstPtr&                            uav_state,
+    const trajectory_msgs::MultiDOFJointTrajectoryPointConstPtr& last_position_cmd)
     override;
 
 private:
@@ -40,28 +40,25 @@ private:
 
   bool m_is_active = false;
 
-  // dodavati stvari ovdje
-
   // constructor variables
-  bool m_verbose_x, m_verbose_y, m_verbose_z;
-  int m_max_iters_x, m_max_iters_y, m_max_iters_z;
+  bool                m_verbose_x, m_verbose_y, m_verbose_z;
+  int                 m_max_iters_x, m_max_iters_y, m_max_iters_z;
   std::vector<double> m_Q_x, m_Q_y, m_Q_z;
   std::vector<double> m_Q_last_x, m_Q_last_y, m_Q_last_z;
-  double m_dt1_x, m_dt2_x, m_p1_x, m_p2_x;
-  double m_dt1_y, m_dt2_y, m_p1_y, m_p2_y;
-  double m_dt1_z, m_dt2_z, m_p1_z, m_p2_z;
+  double              m_dt1_x, m_dt2_x, m_p1_x, m_p2_x;
+  double              m_dt1_y, m_dt2_y, m_p1_y, m_p2_y;
+  double              m_dt1_z, m_dt2_z, m_p1_z, m_p2_z;
 
   // solver variables (states and constraints)
   Eigen::MatrixXd m_initial_state_x, m_reference_x;
-  double m_max_speed_x, m_max_acc_x, m_max_u_x, m_max_du_x, m_u_x;
+  double          m_max_speed_x, m_max_acc_x, m_max_u_x, m_max_du_x, m_u_x;
   Eigen::MatrixXd m_initial_state_y, m_reference_y;
-  double m_max_speed_y, m_max_acc_y, m_max_u_y, m_max_du_y, m_u_y;
+  double          m_max_speed_y, m_max_acc_y, m_max_u_y, m_max_du_y, m_u_y;
   Eigen::MatrixXd m_initial_state_z, m_reference_z;
-  double m_max_speed_z, m_max_acc_z, m_max_u_z, m_max_du_z, m_u_z;
-
+  double          m_max_speed_z, m_max_acc_z, m_max_u_z, m_max_du_z, m_u_z;
 
   // messages from Odometry and Trajectory
-  nav_msgs::Odometry initial_state;
+  nav_msgs::Odometry                            initial_state;
   trajectory_msgs::MultiDOFJointTrajectoryPoint reference;
 
   double odom_pos_x, odom_pos_y, odom_pos_z;
@@ -71,15 +68,14 @@ private:
   double ref_spd_x, ref_spd_y, ref_spd_z;
   double ref_acc_x, ref_acc_y, ref_acc_z;
 
-
   // control loop variables
   mavros_msgs::AttitudeTarget attitude_target;
 
   // orientation
   geometry_msgs::Quaternion ref_orientation_Q, des_orientation_Q;
-  Eigen::Quaternionf eig_ref_orientation_Q, eig_des_orientation_Q;
-  Eigen::Vector3f euler;
-  double roll, pitch, yaw;
+  Eigen::Quaternionf        eig_ref_orientation_Q, eig_des_orientation_Q;
+  Eigen::Vector3f           euler;
+  double                    roll, pitch, yaw;
 
   Eigen::Vector3f Xc, Yc;
   Eigen::Vector3f a_des;
@@ -87,23 +83,15 @@ private:
   Eigen::Matrix3f R_des;
 
   // thrust
-  bool feed_fwd_acc_flag;
-  const double g = 9.81;
-  Eigen::Vector3f Ra, f;
-  double A, B, UAV_mass;
-  int n_motors;
-  double thrust_force, thrust;
+  bool                      feed_fwd_acc_flag;
+  const double              g = 9.81;
+  Eigen::Vector3f           Ra, f;
+  double                    A, B, UAV_mass;
+  int                       n_motors;
+  double                    thrust_force, thrust;
   geometry_msgs::Quaternion R_Q;
-  Eigen::Quaternionf eig_R_Q;
-  Eigen::Matrix3f R;
-
-
-
-
-
-
-
-
+  Eigen::Quaternionf        eig_R_Q;
+  Eigen::Matrix3f           R;
 };
 }// namespace uav_ros_control
 
