e016782a1081e52b6d502a42ea1969be66c2d234
Lovro Markovic <lovro.markovic@fer.hr>
2021-07-07 22:26:38 +0200

    [uav_util::automatic_takeoff] Revert to 7bc56bade5eae1a8464bce619efce9d3a6a7865c

--- include/uav_ros_control/util/uav_util.hpp
+++ include/uav_ros_control/util/uav_util.hpp
@@ -7,18 +7,14 @@ namespace uav_util {
 
 /**
  * @brief A function that performs automatic takeoff if using the CarrotReferenceNode.
- *
- * @param nh
+ * 
+ * @param nh 
  * @param relative_altitude Relative takeoff altitude
  * @param enable_carrot Enable carrot mode through joy topic
  * @return true if takeoff successful
  * @return false if takeoff unsuccessful
  */
-bool automatic_takeoff(
-  ros::NodeHandle&   nh,
-  double             relative_altitude,
-  bool               enable_carrot  = true,
-  const std::string& odometry_topic = "mavros/global_position/local");
+bool automatic_takeoff(ros::NodeHandle &nh, double relative_altitude, bool enable_carrot = true);
 
 }// namespace uav_util
 
--- src/util/uav_util.cpp
+++ src/util/uav_util.cpp
@@ -2,17 +2,13 @@
 
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/CommandBool.h>
-#include <mavros_msgs/State.h>
 #include <std_srvs/Empty.h>
 #include <sensor_msgs/Joy.h>
 #include <uav_ros_msgs/TakeOff.h>
-#include <std_msgs/String.h>
-#include <nav_msgs/Odometry.h>
 
-bool uav_util::automatic_takeoff(ros::NodeHandle&   nh,
-                                 double             relative_altitude,
-                                 bool               enable_carrot,
-                                 const std::string& odometry_topic)
+bool uav_util::automatic_takeoff(ros::NodeHandle &nh,
+  double relative_altitude,
+  bool enable_carrot)
 {
   // Initialize mavros/set_mode client
   auto set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
@@ -41,39 +37,6 @@ bool uav_util::automatic_takeoff(ros::NodeHandle&   nh,
   // Initialize Joy publisher
   auto joy_pub = nh.advertise<sensor_msgs::Joy>("joy", 1);
 
-  // Initialize carrot status subscriber
-  std::string carrot_status{ "n/a" };
-  auto        carrot_status_sub = nh.subscribe<std_msgs::String>(
-    "carrot/status", 1, [&](const std_msgs::StringConstPtr& msg) {
-      carrot_status = msg->data;
-    });
-
-  // Initialize mavros state subscriber
-  bool is_armed         = false;
-  auto mavros_state_sub = nh.subscribe<mavros_msgs::State>(
-    "mavros/state", 1, [&](const mavros_msgs::StateConstPtr& msg) {
-      is_armed = msg->armed;
-    });
-
-  // Setup odometry subscriber
-  nav_msgs::Odometry uav_odom;
-  auto               odometry_sub = nh.subscribe<nav_msgs::Odometry>(
-    odometry_topic, 1, [&](const nav_msgs::OdometryConstPtr& msg) { uav_odom = *msg; });
-
-  const auto shutdown_connections = [&]() {
-    // Shutdown all clients
-    set_mode_client.shutdown();
-    arming_client.shutdown();
-    takeoff_client.shutdown();
-
-    // Shutdown pub
-    joy_pub.shutdown();
-
-    // Shutdown sub
-    carrot_status_sub.shutdown();
-    mavros_state_sub.shutdown();
-  };
-
   // start automatic takeoff
   ROS_INFO("[uav_util::automatic_takeoff] - started");
   ros::Duration wait(2.0);
@@ -81,8 +44,8 @@ bool uav_util::automatic_takeoff(ros::NodeHandle&   nh,
   // Swith to GUIDED mode
   mavros_msgs::SetMode guided_request;
   guided_request.request.custom_mode = "GUIDED";
-  guided_request.request.base_mode   = 0;
-  auto call_success                  = set_mode_client.call(guided_request);
+  guided_request.request.base_mode = 0;
+  auto call_success = set_mode_client.call(guided_request);
   if (!call_success || !guided_request.response.mode_sent) {
     ROS_WARN("[uav_util::automatic_takeoff] - unable to switch to GUIDED.");
     return false;
@@ -95,84 +58,43 @@ bool uav_util::automatic_takeoff(ros::NodeHandle&   nh,
     sensor_msgs::Joy enable_carrot_msg;
     enable_carrot_msg.buttons = std::vector<int>{ 0, 0, 0, 0, 0, 1 };
     joy_pub.publish(enable_carrot_msg);
-    wait.sleep();
-  }
 
-  int fail_counter = 0;
-
-
-  // Wait loop for carrot reference activation
-  while (ros::ok()) {
-    ros::spinOnce();
-    if (carrot_status == "CARROT_ON_LAND") { break; }
-    fail_counter++;
-    ROS_WARN_STREAM("[uav_util::automatic_takeoff] - failed to activate CarrotReference ["
-                    << fail_counter << "/10]");
-    if (fail_counter >= 10) {
-      shutdown_connections();
-      return false;
-    }
     wait.sleep();
   }
 
   // Arm the UAV
   mavros_msgs::CommandBool arm_command;
   arm_command.request.value = true;
-  call_success              = arming_client.call(arm_command);
+  call_success = arming_client.call(arm_command);
   if (!call_success) {
     ROS_WARN_STREAM("[uav_util::automatic_takeoff] - call to "
                     << arming_client.getService() << " service failed.");
-    shutdown_connections();
     return false;
   }
 
-  // Wait for armed
-  fail_counter = 0;
-  while (!is_armed && ros::ok()) {
-    ros::spinOnce();
-    fail_counter++;
-    ROS_WARN_STREAM("[uav_util::automatic_takeoff] - failed to arm the UAV ["
-                    << fail_counter << "/10]");
-    if (fail_counter >= 10) {
-      shutdown_connections();
-      return false;
-    }
-    wait.sleep();
-  }
+  wait.sleep();
 
   uav_ros_msgs::TakeOff takeoff_request;
   takeoff_request.request.rel_alt = relative_altitude;
-  call_success                    = takeoff_client.call(takeoff_request);
+  call_success = takeoff_client.call(takeoff_request);
   if (!call_success) {
     ROS_WARN("[uav_util::automatic_takeoff] - call to takeoff service unsuccessful.");
-    shutdown_connections();
     return false;
   }
   if (!takeoff_request.response.success) {
     ROS_WARN_STREAM("[uav_util::automatic_takeoff] - takeoff failed with response: "
                     << takeoff_request.response);
-    shutdown_connections();
     return false;
   }
 
-  // Wait for takeoff
-  fail_counter = 0;
-  while (ros::ok()) {
-    ros::spinOnce();
-    if (abs(uav_odom.pose.pose.position.z - relative_altitude) < 0.5) {
-      ROS_INFO(
-        "[uav_util::automatic_takeoff] - UAV is deffinitely not on the ground anymore!.");
-      break;
-    }
-    fail_counter++;
-    if (fail_counter >= 10) {
-      shutdown_connections();
-      return false;
-    }
-    wait.sleep();
-  }
-
   ROS_INFO("[uav_util::automatic_takeoff] - takeoff successful!.");
-  shutdown_connections();
+  // Shutdown all clients
+  set_mode_client.shutdown();
+  arming_client.shutdown();
+  takeoff_client.shutdown();
+
+  // Shutdown pub
+  joy_pub.shutdown();
+
   return true;
 }
